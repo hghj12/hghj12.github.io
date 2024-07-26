cbc45abc917620addf9cce9e34358dba8c81a094
David Park <dedikpak333@gmail.com>
2024-07-26 15:13:22 +0600

    add frontenf

---
+++ frontend2024/code.js
@@ -0,0 +1,85 @@
+//const s = prompt();
+//const arr = s.split(" ")
+//for(let i = 0; i < arr.length - 1; i += 2){
+	//let temp = arr[i];
+	//arr [i] = arr[i+1]
+	//arr[i+1] = temp
+//}
+//console.log(arr);
+
+//const s = prompt();
+//const a = s.split(" ");
+//const uniq = [];
+
+//for (const element of a){
+	//if (uniq.indexOf(element) === -1){
+		//uniq.push(element);
+	//}
+//}
+//console.log(uniq.length);
+
+//(tempCelsius) => {
+	//const tempFahrenheit = tempCelsius *9/5 + 32;
+	//return tempFahrenheit
+//}
+
+//const c2f = (temp) => {
+	//const Fahrenheit = temp *9/5 + 32;
+	//return Fahrenheit;
+//};
+
+		//const arrC2F = (arr) => {
+			//let arrF = [];
+			//for (let i = 0; i < arr.length; i++)
+				//arrF.push(arr[i] * 9/5 + 32);
+		//}
+		//return arrF;
+
+		//const arrf2c = (arr) => {
+			//let arrC = [];
+			//for (let i = 0; i < arr.length; i++){
+				//newArr.push((arr[i] - 32)* 5/9);
+			//}
+			//return newArr;
+		//}
+		//const c2k = (temp) => {
+   	//return temp + 273.15;
+	//};
+//const distnance = (a, b) => {
+   // if (a >=b) {
+        //return a - b; }
+   // else {
+       // return b - a;
+    //}
+//};
+//const c2k||f2c||c2f = (temp) => {
+   // return temp + 273.15;
+//};
+//let arr = [1,2,3,4,5,,6];
+//for (let idx = 0; idx < arr.length; idx +=1) {
+	//arr [ind]= f2c(arr[idx]);
+//}
+//console.log(arr);
+
+//const modifyArray = (arr, transformFunc) =>{
+	//let newArr = [];
+	//for (let i = 0; i < arr.length; i++) {
+		//newArr.push(transformFunc(arr[i]));
+	//}
+	//return newArr;
+//}
+
+//const c2k = (temp) => {
+  // return temp + 273.15;
+//};
+//const f2c = (temp) => {
+  // return temp *5/9 - 32;
+//};
+//const myArray = [1,2,3,4,5];
+//const newArray = myArray.map((num) => (num*3));
+
+//let value = [3, -6, 2, 0, -9, 4];
+//let positiveValues = values.filter(x => x > 0);
+
+//const staffNames = ["Mark", "Andrew", "Andy", "Kenny", "Helen", "Tony"];
+//const validStaffNames = staffNames.filter((staffName) => staffName !== "Kenny")
